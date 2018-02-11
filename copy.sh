92e8452ee77c5908e2baa2645540e71fa5b7d2cb
tcking <mytcking@gmail.com>
2018-02-11 16:45:13 +0800

    init commit

--- copy.sh
+++
@@ -1,1 +0,0 @@
-cp -R ../ijkplayer_tc/android/ijkplayer/ijkplayer-*/src/main/libs/* 0.1.17
---
+++ init.sh
@@ -0,0 +1,14 @@
+playerVersion=0.1.17
+rm -r $playerVersion
+mkdir $playerVersion
+cp -R ../ijkplayer_tc/android/ijkplayer/ijkplayer-*/src/main/libs/* $playerVersion
+for file in ./$playerVersion/*
+do
+	if test -d $file
+		then 
+		echo $file
+		zip $file/so.zip -j $file/*
+		rm $file/*.so
+	fi
+done
+
